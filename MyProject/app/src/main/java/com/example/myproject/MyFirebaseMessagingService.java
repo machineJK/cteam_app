8d28ef76bc4c58d657c41342a1860ce1d7efe007
302-14\hanul <witlim1225@gmail.com>
2021-02-19 16:11:03 +0900

    알람 끝! (어플 켜져있을때 울리는 알람은 어떻게 처리할 것인가)

--- MyProject/app/src/main/java/com/example/myproject/MyFirebaseMessagingService.java
+++ MyProject/app/src/main/java/com/example/myproject/MyFirebaseMessagingService.java
@@ -1,11 +1,17 @@
 package com.example.myproject;
 
-import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Build;
+import android.util.Log;
 
+import androidx.annotation.NonNull;
 import androidx.core.app.NotificationCompat;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
@@ -13,52 +19,99 @@ import com.google.firebase.messaging.RemoteMessage;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
-    @Override
-    public void onMessageReceived(RemoteMessage remoteMessage) {
-        if (remoteMessage != null && remoteMessage.getData().size() > 0) {
-            sendNotification(remoteMessage);
+        //푸시 알림 설정
+        private String title = "";
+        private String name = "";
+        private String body = "";
+        private String color = "";
+
+        @Override
+        public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+            super.onMessageReceived(remoteMessage);
+
+            //푸시알림 메세지 분기
+            //putData를 사용했을때 data가져오기
+            if(remoteMessage.getData().size() > 0){
+                title = remoteMessage.getData().get("title");
+                name = remoteMessage.getData().get("name");
+                body = remoteMessage.getData().get("body");
+                color = remoteMessage.getData().get("color");
+
+                if(true){
+                    scheduleJob();
+                }else{
+                    hadleNow();
+                }
+            }
+
+            //Notification 사용했을때 data 가져오기
+            if(remoteMessage.getNotification() != null){
+                Log.d("메세지", "Message Notification Body: " + remoteMessage.getNotification().getBody());
+            }
+            sendNotification();
         }
-    }
 
-    private void sendNotification(RemoteMessage remoteMessage) {
-
-        String title = remoteMessage.getData().get("title");
-        String message = remoteMessage.getData().get("message");
-
-        final String CHANNEL_ID = "ChannelID";
-        NotificationManager mManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            final String CHANNEL_NAME = "ChannerName";
-            final String CHANNEL_DESCRIPTION = "ChannerDescription";
-            final int importance = NotificationManager.IMPORTANCE_HIGH;
-
-            // add in API level 26
-            NotificationChannel mChannel = new NotificationChannel(CHANNEL_ID, CHANNEL_NAME, importance);
-            mChannel.setDescription(CHANNEL_DESCRIPTION);
-            mChannel.enableLights(true);
-            mChannel.enableVibration(true);
-            mChannel.setVibrationPattern(new long[]{100, 200, 100, 200});
-            mChannel.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
-            mManager.createNotificationChannel(mChannel);
+        @Override
+        public void onNewToken(@NonNull String token) {
+            super.onNewToken(token);
+            sendRegistrationToServer(token);
         }
 
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID);
-        builder.setSmallIcon(R.drawable.ic_launcher_background);
-        builder.setAutoCancel(true);
-        builder.setDefaults(Notification.DEFAULT_ALL);
-        builder.setWhen(System.currentTimeMillis());
-        builder.setSmallIcon(R.mipmap.ic_launcher);
-        builder.setContentTitle(title);
-        builder.setContentText(message);
-        if(Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
-            builder.setContentTitle(title);
-            builder.setVibrate(new long[]{500, 500});
+        private void scheduleJob() {
+//        Constraints constraints = new Constraints.Builder()
+//                .setRequiredNetworkType(NetworkType.UNMETERED)
+//                .setRequiresCharging(true)
+//                .build();
+//
+//        PeriodicWorkRequest work = new PeriodicWorkRequest
+//                .Builder(AteamWorker.class,15, TimeUnit.MINUTES)
+//                .setConstraints(constraints)
+//                .setBackoffCriteria(BackoffPolicy.LINEAR,
+//                        OneTimeWorkRequest.MAX_BACKOFF_MILLIS,
+//                        TimeUnit.MINUTES)
+//                .build();
+//        WorkManager.getInstance(this).enqueue(work);
         }
-        mManager.notify(0, builder.build());
-    }
 
-    @Override
-    public void onNewToken(String s) {
-        super.onNewToken(s);
-    }
+        private void hadleNow() {
+
+        }
+
+        private void sendRegistrationToServer(String token) {
+            //앱 서버에 토큰을 보내려면 이 메서드를 구현한다.
+        }
+
+        private void sendNotification() {
+            Intent intent = new Intent(this, LoginActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            PendingIntent pendingIntent = PendingIntent.getActivity(this, 2000 /* Request code */, intent,
+                    PendingIntent.FLAG_ONE_SHOT);
+
+            String channelId = "Tutors";
+            Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+            NotificationCompat.Builder notificationBuilder =
+                    new NotificationCompat.Builder(this, channelId)
+                            .setSmallIcon(R.mipmap.ic_launcher_round)
+                            .setContentTitle(name)
+                            .setContentText(body)
+                            .setColor(Color.parseColor(color))
+                            .setAutoCancel(true)
+                            .setSound(defaultSoundUri)
+                            .setContentIntent(pendingIntent)
+                            .setPriority(NotificationCompat.PRIORITY_HIGH);
+
+            NotificationManager notificationManager =
+                    (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+            // Since android Oreo notification channel is needed.
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                NotificationChannel channel = new NotificationChannel(channelId,
+                        "Channel human readable title",
+                        NotificationManager.IMPORTANCE_DEFAULT);
+                notificationManager.createNotificationChannel(channel);
+            }
+
+            notificationManager.notify(2000 /* ID of notification */, notificationBuilder.build());
+        }
+
 }
